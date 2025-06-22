33554f77958df91ce891bd074bafd618a6499ffe
Karina-Jannette <103295844+Karina-Jannette@users.noreply.github.com>
2025-06-22 15:58:16 -0600

    style: se actualizan iconos en page principal

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,6 @@
 'use client'
 
+import { FaFacebook, FaInstagram, FaWhatsapp } from "react-icons/fa";
 import Carrusel from "../components/ui/Carrusel";
 import Card from "../components/ui/EducationCard";
 import Link from "next/link";
@@ -419,13 +420,7 @@ export default function Home() {
                     className="flex flex-col items-center hover:scale-110 transition duration-300"
                   >
                     <div className="w-16 h-16 rounded-full bg-blue-100 flex items-center justify-center mb-2">
-                      <svg
-                        className="w-8 h-8 text-blue-600"
-                        fill="currentColor"
-                        viewBox="0 0 24 24"
-                      >
-                        <path d="M24 12.073c0-6.627-5.373-12-12-12s-12 5.373-12 12c0 5.99 4.388 10.954 10.125 11.854v-8.385H7.078v-3.47h3.047V9.43c0-3.007 1.792-4.669 4.533-4.669 1.312 0 2.686.235 2.686.235v2.953H15.83c-1.491 0-1.956.925-1.956 1.874v2.25h3.328l-.532 3.47h-2.796v8.385C19.612 23.027 24 18.062 24 12.073z" />
-                      </svg>
+                      <FaFacebook size={28} /> 
                     </div>
                     <span className="text-gray-700 font-medium">Facebook</span>
                   </a>
@@ -436,13 +431,7 @@ export default function Home() {
                     className="flex flex-col items-center hover:scale-110 transition duration-300"
                   >
                     <div className="w-16 h-16 rounded-full bg-pink-50 flex items-center justify-center mb-2">
-                      <svg
-                        className="w-8 h-8 text-pink-600"
-                        fill="currentColor"
-                        viewBox="0 0 24 24"
-                      >
-                        <path d="M12.017 0C5.396 0 .029 5.367.029 11.987c0 6.62 5.367 11.987 11.988 11.987c6.62 0 11.987-5.367 11.987-11.987C24.014 5.367 18.637.001 12.017.001zM8.449 16.988c-1.297 0-2.048-.611-2.048-1.624v-4.65H4.292v-1.381h2.109V7.558c0-2.05 1.247-3.148 3.062-3.148c.871 0 1.618.065 1.836.094v2.13l-1.26.001c-.987 0-1.178.469-1.178 1.156v1.516h2.352l-.306 1.381h-2.046v4.65c0 1.013-.751 1.624-2.048 1.624H8.449z" />
-                      </svg>
+                      <FaInstagram size={28} />
                     </div>
                     <span className="text-gray-700 font-medium">Instagram</span>
                   </a>
@@ -453,14 +442,7 @@ export default function Home() {
                     className="flex flex-col items-center hover:scale-110 transition duration-300"
                   >
                     <div className="w-16 h-16 rounded-full bg-green-50 flex items-center justify-center mb-2">
-                      <svg
-                        xmlns="http://www.w3.org/2000/svg"
-                        className="h-8 w-8 text-green-600"
-                        fill="currentColor"
-                        viewBox="0 0 24 24"
-                      >
-                        <path d="M.057 24l1.687-6.163c-1.041-1.804-1.588-3.849-1.587-5.946.003-6.556 5.338-11.891 11.893-11.891 3.181.001 6.167 1.24 8.413 3.488 2.245 2.248 3.481 5.236 3.48 8.414-.003 6.557-5.338 11.892-11.893 11.892-1.99-.001-3.951-.5-5.688-1.448l-6.305 1.654zm6.597-3.807c1.676.995 3.276 1.591 5.392 1.592 5.448 0 9.886-4.434 9.889-9.885.002-5.462-4.415-9.89-9.881-9.892-5.452 0-9.887 4.434-9.889 9.884-.001 2.225.651 3.891 1.746 5.634l-.999 3.648 3.742-.981zm11.387-5.464c-.074-.124-.272-.198-.57-.347-.297-.149-1.758-.868-2.031-.967-.272-.099-.47-.149-.669.149-.198.297-.768.967-.941 1.165-.173.198-.347.223-.644.074-.297-.149-1.255-.462-2.39-1.475-.883-.788-1.48-1.761-1.653-2.059-.173-.297-.018-.458.13-.606.134-.133.297-.347.446-.521.151-.172.2-.296.3-.495.099-.198.05-.372-.025-.521-.075-.148-.669-1.611-.916-2.206-.242-.579-.487-.501-.669-.51l-.57-.01c-.198 0-.52.074-.792.372s-1.04 1.016-1.04 2.479 1.065 2.876 1.213 3.074c.149.198 2.095 3.2 5.076 4.487.709.306 1.263.489 1.694.626.712.226 1.36.194 1.872.118.571-.085 1.758-.719 2.006-1.413.248-.695.248-1.29.173-1.414z" />
-                      </svg>
+                      <FaWhatsapp size={28}/>
                     </div>
                     <span className="text-gray-700 font-medium">WhatsApp</span>
                   </a>
@@ -484,7 +466,7 @@ export default function Home() {
                       fill="currentColor"
                       viewBox="0 0 24 24"
                     >
-                      <path d="M12.017 0C5.396 0 .029 5.367.029 11.987c0 6.62 5.367 11.987 11.988 11.987c6.62 0 11.987-5.367 11.987-11.987C24.014 5.367 18.637.001 12.017.001zM8.449 16.988c-1.297 0-2.048-.611-2.048-1.624v-4.65H4.292v-1.381h2.109V7.558c0-2.05 1.247-3.148 3.062-3.148c.871 0 1.618.065 1.836.094v2.13l-1.26.001c-.987 0-1.178.469-1.178 1.156v1.516h2.352l-.306 1.381h-2.046v4.65c0 1.013-.751 1.624-2.048 1.624H8.449z" />
+                      <FaInstagram size={25}/>
                     </svg>
                     <p className="text-gray-500 mb-4">
                       Síguenos en Instagram para ver nuestras publicaciones
